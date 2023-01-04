64d4dcc8b30693861fcfcc77b6422312ee47090e
rays1410 <rays1410@gmail.com>
2023-01-04 19:11:47 +0300

    The header is created

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import headerStyles from "./app-header.module.css";
+import "@ya.praktikum/react-developer-burger-ui-components";
+import { Logo } from "@ya.praktikum/react-developer-burger-ui-components/dist/ui/logo";
+import {
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+const AppHeader = () => {
+  return (
+    <header role="banner" className={headerStyles.header}>
+      <div className={headerStyles.headerContent}>
+        <div className={headerStyles.headerLeft}>
+          <div className={headerStyles.headerItem}>
+            <BurgerIcon type="primary" />
+            <a
+              href="/"
+              className={`${headerStyles.linkStyle} text text_type_main-default`}
+            >
+              Конструктор
+            </a>
+          </div>
+
+          <div className={headerStyles.headerItem}>
+            <ListIcon type="primary" />
+            <a
+              href="/"
+              className={`${headerStyles.linkStyle} text text_type_main-default`}
+            >
+              Лента заказов
+            </a>
+          </div>
+        </div>
+
+        <div className={headerStyles.headerLogo}>
+          <Logo />
+        </div>
+
+        <div className={headerStyles.headerRight}>
+          <div className={headerStyles.headerItem}>
+            <ProfileIcon type="primary" />
+            <a
+              href="/"
+              className={`${headerStyles.linkStyle} text text_type_main-default`}
+            >
+              Личный кабинет
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
---
+++ src/components/app/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../../images/logo.svg";
+import AppHeader from "../app-header/app-header";
+
+function App() {
+  return (
+    <AppHeader/>
+  );
+}
+
+export default App;
